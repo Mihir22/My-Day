819594b380828d658bacaa6d585eccd76a1ebb46
Mihir Mesia <mihirmesia@mihirs-macbook-air.local>
2018-10-31 21:19:57 +0530

    Persisting data using Codable Protocol and Plist

--- My Day/Controllers/MyDayListViewController.swift
+++ My Day/Controllers/MyDayListViewController.swift
@@ -8,43 +8,44 @@
 
 import UIKit
 
-class MyDayListViewController: UITableViewController {
+class MyDayListViewController: UITableViewController{
     
    var itemArray = [Item]()
-  let defaluts = UserDefaults.standard
+  
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadItems()
+        
+        print(dataFilePath!)
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaluts.array(forKey: "MyDayListArray") as? [Item]
-        {
-            itemArray = items
-        }
+        
     }
+    
     //MARK-TableViewDataSource Methods
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyDayItemCell", for: indexPath)
+            
     
-        let item = itemArray[indexPath.row]
+            let item = itemArray[indexPath.row]
         
         cell.accessoryType = item.done ? .checkmark : .none
         return cell
+            
     }
     //MARK - TableViewDelegate methods
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+       override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row].title)
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none   }
-            else{
-                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+      saveItems()
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
 }
@@ -60,14 +61,41 @@ class MyDayListViewController: UITableViewController {
             newItem.title = textField.text!
             self.itemArray.append(newItem)
             self.tableView.reloadData()
-            self.defaluts.set(self.itemArray, forKey: "MyDayListArray")
+            
+            self.saveItems()
+            
+          
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
             textField = alertTextField
+        
         }
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        }
+        catch{
+            
+        }
+        self.tableView.reloadData()
+    }
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+            itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch {
+                print("error decoding item array\(error)")
+            }
+        }
+    }
+
 }
